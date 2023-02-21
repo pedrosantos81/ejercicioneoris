e37f1cb8c85f1bfcd6a6883cc1d121f8a8d4204c
Pedro Evaristo <peterfes20@gmail.com>
2023-02-21 13:04:36 -0600

    Se hizo cambios en test unitarios

--- src/test/java/com/controller/CuentaControllerTest.java
+++ src/test/java/com/controller/CuentaControllerTest.java
@@ -41,72 +41,72 @@ import com.service.CuentaService;
 
 @ActiveProfiles({ "test" })
 
-//@ContextConfiguration(classes = { EjercicioneorisApplication.class })
-//@AutoConfigureMockMvc
-//
-//@SpringBootTest
+@ContextConfiguration(classes = { EjercicioneorisApplication.class })
+@AutoConfigureMockMvc
+
+@SpringBootTest
 class CuentaControllerTest {
 
-//	@Autowired
-//	private MockMvc mvc;
-//
-//	@MockBean
-//	private CuentaService cuentaService;
-//
-//	@Autowired
-//	private ObjectMapper objectMapper;
-//
-//	@BeforeEach
-//	void setUp() throws Exception {
-//	}
-//
-//	@Test
-//	void testDetalleCuentas() throws Exception {
-//		// Given
-//		Cuenta c = Datos.crearCuenta001().get();
-//		c.setIdcliente(528);
-//		c.setCliente(Datos.crearCliente001().get());
-//		List<Cuenta> listaCuentas = new ArrayList<Cuenta>();
-//		listaCuentas.add(c);
-//		System.out.println(objectMapper.writeValueAsString(listaCuentas));
-//
-//		when(cuentaService.getCuentasByCliente(528)).thenReturn(listaCuentas);
-//
-//		// When
-//		mvc.perform(get("/cuentas/{idcliente}", 528).contentType(MediaType.APPLICATION_JSON))
-//				// Then
-//				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON)).andDo(print())
-//				.andExpect(jsonPath("$[0].clienteName").value("Jorge Lopez"))
-//				.andExpect(jsonPath("$[0].saldoinicial").value("3400.0"))
-//				.andExpect(jsonPath("$[0].idcliente", is(528)));
-//
-//		verify(cuentaService).getCuentasByCliente(528);
-//	}
-//
-//	@Test
-//	void testCrearCuenta() throws Exception {
-//		Cuenta cuenta = new Cuenta(TipoCuenta.AHORRO, 3000, true);
-//		cuenta.setCliente(Datos.crearCliente001().get());
-//
-//		when(cuentaService.save(any())).then(invocation -> {
-//			Cuenta c = invocation.getArgument(0);
-//			c.setNumerocuenta(1);
-//			return c;
-//		});
-//
-//		System.out.println(objectMapper.writeValueAsString(cuenta));
-//
-//		// when
-//		mvc.perform(post("/cuentas/{idcliente}", 528).contentType(MediaType.APPLICATION_JSON)
-//				.content(objectMapper.writeValueAsString(cuenta)))
-//				// Then
-//				.andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//				.andDo(print()).andExpect(jsonPath("$.cuenta.numerocuenta", is(1)));
-////                .andExpect(jsonPath("$.cliente.genero").value("MASCULINO"))
-////                .andExpect(jsonPath("$.cliente.nombre").value("Jorge Lopez"));
-//
-//		verify(cuentaService).save(any());
-//
-//	}
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private CuentaService cuentaService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testDetalleCuentas() throws Exception {
+		// Given
+		Cuenta c = Datos.crearCuenta001().get();
+		c.setIdcliente(528);
+		c.setCliente(Datos.crearCliente001().get());
+		List<Cuenta> listaCuentas = new ArrayList<Cuenta>();
+		listaCuentas.add(c);
+		System.out.println(objectMapper.writeValueAsString(listaCuentas));
+
+		when(cuentaService.getCuentasByCliente(528)).thenReturn(listaCuentas);
+
+		// When
+		mvc.perform(get("/cuentas/{idcliente}", 528).contentType(MediaType.APPLICATION_JSON))
+				// Then
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(jsonPath("$[0].clienteName").value("Jorge Lopez"))
+				.andExpect(jsonPath("$[0].saldoinicial").value("3400.0"))
+				.andExpect(jsonPath("$[0].idcliente", is(528)));
+
+		verify(cuentaService).getCuentasByCliente(528);
+	}
+
+	@Test
+	void testCrearCuenta() throws Exception {
+		Cuenta cuenta = new Cuenta(TipoCuenta.AHORRO, 3000, true);
+		cuenta.setCliente(Datos.crearCliente001().get());
+
+		when(cuentaService.save(any())).then(invocation -> {
+			Cuenta c = invocation.getArgument(0);
+			c.setNumerocuenta(1);
+			return c;
+		});
+
+		System.out.println(objectMapper.writeValueAsString(cuenta));
+
+		// when
+		mvc.perform(post("/cuentas/{idcliente}", 528).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(cuenta)))
+				// Then
+				.andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(jsonPath("$.cuenta.numerocuenta", is(1)));
+//                .andExpect(jsonPath("$.cliente.genero").value("MASCULINO"))
+//                .andExpect(jsonPath("$.cliente.nombre").value("Jorge Lopez"));
+
+		verify(cuentaService).save(any());
+
+	}
 
 }
